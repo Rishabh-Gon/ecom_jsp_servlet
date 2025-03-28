5c3fd2f10033abab4f42f33f76ddc0e05b5cec5a
Rishabh Gon <gonrishabh@gmail.com>
2025-03-29 01:17:15 +0530

    Update DBConn.java

--- src/java/dao/DBConn.java
+++ src/java/dao/DBConn.java
@@ -8,11 +8,13 @@ public class DBConn{
 		Connection conn=null;
 		String db_user="root";
 		String db_password="Rishabh@02";
+		String db_name="ecom";
+		String hostname="jdbc:mysql://localhost:3306/";
 		try {	
                     Class.forName("com.mysql.cj.jdbc.Driver");
                     System.out.println("Driver class loaded");
                     System.out.println("Conn is now null");
-                    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ecom?useSSL=false&allowPublicKeyRetrieval=true",
+                    conn = DriverManager.getConnection(hostname+db_name+"?useSSL=false&allowPublicKeyRetrieval=true",
                             db_user, db_password);
                     System.out.println("JDBC Connection established successfully.");
                 } catch (Exception ex) {
